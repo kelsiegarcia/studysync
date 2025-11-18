37f746ea7a993f60c0a5a67c1fe6c65052eab10d
mnhthompson <mnhthompson@gmail.com>
2025-11-17 20:47:38 -0700

    swag fix 4

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
-
 const express = require('express');
 const router = express.Router();
-const swaggerRouter = require('./swagger');
 
-router.use('/api-docs', swaggerRouter);
+
+router.use('/api-docs', require('./swagger'));
 
 module.exports = router;
