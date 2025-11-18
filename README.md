4662aac84f4b862ee5ac3482066862e77e951648
mnhthompson <mnhthompson@gmail.com>
2025-11-17 20:14:57 -0700

    123 routes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # studysync
 Project Idea: "StudySync" An app that connects students with shared study goals. Users can create study sessions, share notes, and track progress. Features: User profiles, session scheduling, progress tracking. Database: Users (ID, name, email, subjects) Sessions (ID, topic, time, participants)
+
+https://studysync-2sto.onrender.com
--- server.js
+++ server.js
@@ -10,6 +10,12 @@ app.get('/', (req, res) => {
     res.send('Hello from Express!');
 });
 
+app.use('/session', require('./routes/session'));
+app.use('/progress', require('./routes/progress'));
+app.use('/note', require('./routes/note'));
+app.use('/user', require('./routes/user'));
+app.use('/', require('./routes'));
+
 // Start server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
