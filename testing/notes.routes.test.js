376a90beeb79e31700ca4585e050a38735e385fd
mnhthompson <mnhthompson@gmail.com>
2025-11-24 09:14:16 -0700

    123

--- testing/notes.routes.test.js
+++ testing/notes.routes.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('server.js'); 
+const app = require('../server.js'); 
 const mongoose = require('mongoose');
 
 describe('Notes Routes', () => {
