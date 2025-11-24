fa3deafeafc1c510dee34f970334e482eb69107f
mnhthompson <mnhthompson@gmail.com>
2025-11-24 09:36:01 -0700

    fake Oauth

--- testing/notes.routes.test.js
+++ testing/notes.routes.test.js
@@ -1,6 +1,17 @@
 const request = require('supertest');
 const app = require('../server.js'); 
 const mongoose = require('mongoose');
+const mongodb = require('../db/connect');
+
+jest.mock('../middleware/ensureAuth', () => (req, res, next) => next());
+
+
+beforeAll((done) => {
+  mongodb.initDb((err) => {
+    if (err) return done(err);
+    done();
+  });
+});
 
 describe('Notes Routes', () => {
   let createdNoteId;
--- testing/swagger.routes.test.js
+++ testing/swagger.routes.test.js
@@ -1,3 +1,14 @@
 const request = require('supertest');
 const app = require('../server.js'); 
 const mongoose = require('mongoose');
+const mongodb = require('../db/connect');
+
+jest.mock('../middleware/ensureAuth', () => (req, res, next) => next());
+
+
+beforeAll((done) => {
+  mongodb.initDb((err) => {
+    if (err) return done(err);
+    done();
+  });
+});
